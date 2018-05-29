874f207d39cf618bb1a05d403d0194bedc6568c1
Newton Galiza <newtonjgaliza@gmail.com>
2018-05-29 17:08:31 -0300

    Create simplefilereader.py

---
+++ simplefilereader.py
@@ -0,0 +1,10 @@
+»> f = open("file.txt", "r")
+»> f.read()
+'hello, its me'
+»> f = open("file.txt", "w")
+»> f.write("bye, bye")
+»> f.close()
+»> f = open("file.txt", "r")
+»> f.read()
+'bye, bye'
+»> f.close()
